40f025f429c3621288e8aecfee633f608a58f61c
Yakiv Vakoliuk <yahacomua@gmail.com>
2019-08-05 09:24:04 +0300

    Telegram requests to cURL

--- class/Telegram.php
+++ class/Telegram.php
@@ -11,7 +11,7 @@ class Telegram {
         ];
         if (!empty($reply_markup))
             $data['reply_markup'] = $reply_markup;
-        return json_decode(file_get_contents(TG_API_URL . "sendMessage?" . http_build_query($data)));
+        return self::send_request('sendMessage', $data);
     }
 
     static public function send_chat_action($chat_id, $action) {
@@ -19,7 +19,7 @@ class Telegram {
             'chat_id' => $chat_id,
             'action' => $action
         ];
-        file_get_contents(TG_API_URL . "sendChatAction?" . http_build_query($data));
+        self::send_request('sendChatAction', $data);
     }
 
     static public function get_keyboard_markup($buttons) {
@@ -44,7 +44,7 @@ class Telegram {
         ];
         if (!empty($reply_markup))
             $data['reply_markup'] = $reply_markup;
-        return json_decode(file_get_contents(TG_API_URL . "editMessageText?" . http_build_query($data)));
+        return self::send_request('editMessageText', $data);
     }
 
     static public function send_document($chat_id, $caption, $document) {
@@ -54,19 +54,26 @@ class Telegram {
             'document' => new CURLFile(realpath($document)),
             'thumb' => new CURLFile(realpath($_SERVER['DOCUMENT_ROOT'] . "/stravex/assets/images/st-gpx.jpg"))
         ];
+        return self::send_request('sendDocument', $data, 'POST');
+    }
 
-        $url = TG_API_URL . "sendDocument";
-
+    static private function send_request($call, $data, $method = 'GET') {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type:multipart/form-data"
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        if ($method === 'GET')
+            $call .= '?' . http_build_query($data);
+        elseif ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        }
+        if ($call === 'sendDocument')
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                "Content-Type:multipart/form-data"
+            ]);
+        curl_setopt($ch, CURLOPT_URL, TG_API_URL . $call);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-        $output = curl_exec($ch);
+        $response = curl_exec($ch);
         curl_close($ch);
-        return $output;
+        return $response;
     }
 }
